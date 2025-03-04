85159b5e10a086a462ebee1955e7ee0922449f6e
weilycoder <weily2009@outlook.com>
2025-03-04 15:53:18 +0800

    Add a debugging script and update the build script

---
+++ debug_fleac.sh
@@ -0,0 +1,4 @@
+cd fleac
+flex -o flea.lex.cpp flea.l
+bison -d -o flea.tab.cpp flea.y
+clang++ -o fleac flea.tab.cpp flea.lex.cpp flea_ast.cpp main.cpp -g -std=c++20 -Wall -Wextra -Wpedantic -Wconversion -fsanitize=address
